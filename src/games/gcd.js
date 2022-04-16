ca6e20ca9c4da20772874fce6fada413942154a3
Alexander Kamenskiy <kamenskogo@gmail.com>
2022-04-16 21:57:24 +0300

    [Project]: added GCD game

---
+++ src/games/gcd.js
@@ -0,0 +1,27 @@
+import { getRandomNumber } from '../utils.js';
+
+const RULES = 'Find the greatest common divisor of given numbers.';
+
+const getFullExercise = () => {
+  const a = getRandomNumber(101);
+  const b = getRandomNumber(101);
+
+  const question = `${a} ${b}`;
+  const findGCD = (num1, num2) => {
+    if (num1 % num2 === 0) {
+      return num2;
+    }
+    return findGCD(num2, num1 % num2);
+  };
+
+  const correctAnswer = `${a > b ? findGCD(a, b) : findGCD(b, a)}`;
+
+  return [question, correctAnswer];
+};
+
+const BrainGCD = {
+  RULES,
+  getFullExercise,
+};
+
+export default BrainGCD;
